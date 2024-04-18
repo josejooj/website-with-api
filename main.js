cd2c6e9a61fba0497cc3450ac4143892fdd2dd53
josejooj <josejooj.dev@gmail.com>
2024-04-18 11:03:30 -0300

    feat: now websocket connections work

--- main.js
+++ main.js
@@ -29,6 +29,26 @@ const server = createServer(async (req, res) => {
 
 });
 
+server.on("upgrade", async (req, socket, head) => {
+
+    try {
+
+        let target = targets.website;
+
+        if (req.url.startsWith("/api")) {
+            req.url = req.url.slice(4);
+            target = targets.api;
+        };
+
+        await proxy.ws(req, socket, head, { target });
+        
+    } catch (error) {
+        console.error(error);
+        socket.end();
+    }
+
+});
+
 server.listen(80, () => console.log("A proxy está ativa em http://localhost"));
 
 new Worker("./start_service.js");
